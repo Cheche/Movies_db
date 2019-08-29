ca93a58611ad7bf92889b269ab396725e12f156d
Schweikofski <schweikofski@gmail.com>
2019-08-28 22:46:45 -0300

    Added basic service on home page.

---
+++ src/app/pipes/imagen.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const URL = environment.imgPath;
+
+@Pipe({
+  name: 'imagen'
+})
+
+export class ImagenPipe implements PipeTransform {
+
+  transform(img: string, size: string = 'w500'): string {
+    // no img
+    if ( !img ) {
+      return './assets/no-image-banner.jpg';
+    }
+
+    // basic url img
+    const imgUrl = `${ URL }/${ size }${ img }`;
+
+    return imgUrl;
+  }
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: 'e8c4a626cc42cb08fde8c55032abaa0a',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
